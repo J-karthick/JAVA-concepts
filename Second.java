b64987914a135cce41622cbca795563948f7fe55
J-karthick <81806765+J-karthick@users.noreply.github.com>
2021-07-14 20:32:03 +0530

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,11 @@
+public class Second{
+public static void main(String[] arg)
+{
+	System.out.println("Hi");
+	System.out.println("This is my first lab in java");
+	System.out.println("And this is my register no: 17MIS0207 ");
+	System.out.println(arg[0]);
+
+}
+
+}
---
+++ Sum.java
@@ -0,0 +1,12 @@
+public class Sum{
+	public static void main(String[] arg){
+		int x,y,z;
+		x=24;
+		y=37;
+		z=x+y;
+		System.out.println("The sum of 24 and 37 is:" + z);
+
+	}
+	
+
+}
