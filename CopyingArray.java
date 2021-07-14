1cf475f8be2d408af14dc2db574f9ee6deba402a
J-karthick <81806765+J-karthick@users.noreply.github.com>
2021-07-14 21:05:37 +0530

    Update CopyingArray.java

--- CopyingArray.java
+++ CopyingArray.java
@@ -4,7 +4,7 @@ public class CopyingArray{
         int [] arr1 = new int [] {1, 2, 3, 4, 5};     
         int arr2[] = new int[arr1.length];    
 		
-		System.out.println("17MIS0207 - J.KARTHICK");
+		System.out.println("J.KARTHICK");
         
         for (int i = 0; i < arr1.length; i++) {     
             arr2[i] = arr1[i];     
